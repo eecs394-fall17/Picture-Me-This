5c95cb4b2b195010b47cb0f8328451d7952353f7
brotatotes <erichao2018@u.northwestern.edu>
2017-10-31 21:13:14 -0500

    some DataServiceProvider functions

--- src/providers/data-service/data-service.ts
+++ src/providers/data-service/data-service.ts
@@ -5,6 +5,7 @@ import 'rxjs/add/operator/map';
 import * as firebase from "firebase";
 import 'firebase/firestore';
 import {AngularFirestore} from "angularfire2/firestore";
+import {Garment} from "../../models/garment";
 
 /*
   Generated class for the DataServiceProvider provider.
@@ -19,9 +20,44 @@ import {AngularFirestore} from "angularfire2/firestore";
 @Injectable()
 export class DataServiceProvider {
   public db;
+  public uid; //user id for authentication later
 
   constructor(public http: Http, public afs: AngularFirestore) {
     this.db = firebase.firestore();
+    this.uid = "hello"; //for now, hard code user id. TODO update later
+  }
+
+  getTops() {
+    return this.getClothes("Tops");
+  }
+
+  getBottoms() {
+    return this.getClothes("Bottoms");
+  }
+
+  getShoes() {
+    return this.getClothes("Shoes");
+  }
+
+  addTop(garment: Garment) {
+    this.addClothing("Tops", garment);
+  }
+
+  addBottom(garment: Garment) {
+    this.addClothing("Bottoms", garment);
+  }
+
+  addShoe(garment: Garment) {
+    this.addClothing("Shoes", garment);
+  }
+
+  getClothes(type: string) {
+    return this.db.collection("Users").doc(this.uid).collection(type).get();
+  }
+
+  addClothing(type: string, garment: Garment) {
+    // TODO check if name is already taken?
+    this.db.collection("Users").doc(this.uid).collection(type).set(garment);
   }
 
 }
