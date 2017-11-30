060db59592215c23828e7c3cdfb2e8a66aaf1e78
brotatotes <erichao2018@u.northwestern.edu>
2017-11-29 23:26:24 -0600

    fix wardrobe sliding in progress

--- src/pages/wardrobe/wardrobe.ts
+++ src/pages/wardrobe/wardrobe.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {AlertController, NavController} from 'ionic-angular';
+import {Component, QueryList, ViewChildren} from '@angular/core';
+import {AlertController, NavController, Slides} from 'ionic-angular';
 import 'firebase/firestore';
 import {DataServiceProvider} from "../../providers/data-service/data-service";
 import {OutfitDisplayPage} from '../outfit-display/outfit-display';
@@ -9,6 +9,8 @@ import {OutfitDisplayPage} from '../outfit-display/outfit-display';
   templateUrl: 'wardrobe.html'
 })
 export class WardrobePage {
+  @ViewChildren(Slides) slides: QueryList<Slides>;
+
   garmentsT;
   garmentsB;
   garmentsS;
@@ -16,11 +18,12 @@ export class WardrobePage {
   constructor(public navCtrl: NavController,
               public alertCtrl: AlertController,
               public dsp: DataServiceProvider) {
+
+
     // TODO user key needs to be streamlined, whenever we decide to develop auth.
     // TODO database calls need to be streamlined by creating functions in DataServiceProvider.
     this.garmentsT = this.dsp.afs.collection("Users").doc("hello").collection("Tops").valueChanges();
     this.garmentsB = this.dsp.afs.collection("Users").doc("hello").collection("Bottoms").valueChanges();
-
     this.garmentsS = this.dsp.afs.collection("Users").doc("hello").collection("Shoes").valueChanges();
   }
 
@@ -50,6 +53,37 @@ export class WardrobePage {
         }
       ]
     }).present();
+  }
+
+  ionViewDidLoad() {
+    this.slides.forEach(s => {
+      s.lockSwipes(true);
+    });
+
+    console.log(this.slides);
+    console.log(this.slides.toArray()[0]);
+  }
+
+  slideNext(i) {
+    let s = this.slides.toArray()[i];
+    s.lockSwipes(false);
+    if (s.isEnd()) {
+      s.slideTo(0);
+    } else {
+      s.slideNext();
+    }
+    s.lockSwipes(true);
+  }
+
+  slidePrev(i) {
+    let s = this.slides.toArray()[i];
+    s.lockSwipes(false);
+    if (s.isBeginning()) {
+      s.slideTo(s.length()-1);
+    } else {
+      s.slidePrev();
+    }
+    s.lockSwipes(true);
   }
 
 }
